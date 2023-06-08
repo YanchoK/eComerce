baa472ea647e899af0b2cd8033672f3fe6c41bf9
Yancho Kostov <yankostov1@gmail.com>
2023-06-08 20:51:20 +0300

    Add controllers

---
+++ eComerce/Controllers/CinemaCoontroller.cs
@@ -0,0 +1,21 @@
+﻿using eComerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eComerce.Controllers
+{
+    public class CinemaCoontroller : Controller
+    {
+        private readonly AppDbContext _context;
+        public CinemaCoontroller(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}

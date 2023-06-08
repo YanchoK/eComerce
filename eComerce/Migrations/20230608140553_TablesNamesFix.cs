01c31e07f143fa0d90d5219e21cca7dc1bad1c6d
Yancho Kostov <yankostov1@gmail.com>
2023-06-08 17:14:08 +0300

    Create DB migration - fix table names

---
+++ eComerce/Migrations/20230608140553_TablesNamesFix.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eComerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class TablesNamesFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Actors_ActorId",
+                table: "Actors_Movies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actors_Movies_Movie_MovieId",
+                table: "Actors_Movies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Cinemas_CinemaId",
+                table: "Movie");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cinemas",
+                table: "Cinemas");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actors_Movies",
+                table: "Actors_Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actors",
+                table: "Actors");
+
+            migrationBuilder.RenameTable(
+                name: "Cinemas",
+                newName: "Cinema");
+
+            migrationBuilder.RenameTable(
+                name: "Actors_Movies",
+                newName: "Actor_Movie");
+
+            migrationBuilder.RenameTable(
+                name: "Actors",
+                newName: "Actor");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Actors_Movies_MovieId",
+                table: "Actor_Movie",
+                newName: "IX_Actor_Movie_MovieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cinema",
+                table: "Cinema",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actor_Movie",
+                table: "Actor_Movie",
+                columns: new[] { "ActorId", "MovieId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actor",
+                table: "Actor",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actor_Movie_Actor_ActorId",
+                table: "Actor_Movie",
+                column: "ActorId",
+                principalTable: "Actor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actor_Movie_Movie_MovieId",
+                table: "Actor_Movie",
+                column: "MovieId",
+                principalTable: "Movie",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Cinema_CinemaId",
+                table: "Movie",
+                column: "CinemaId",
+                principalTable: "Cinema",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actor_Movie_Actor_ActorId",
+                table: "Actor_Movie");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Actor_Movie_Movie_MovieId",
+                table: "Actor_Movie");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Cinema_CinemaId",
+                table: "Movie");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cinema",
+                table: "Cinema");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actor_Movie",
+                table: "Actor_Movie");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actor",
+                table: "Actor");
+
+            migrationBuilder.RenameTable(
+                name: "Cinema",
+                newName: "Cinemas");
+
+            migrationBuilder.RenameTable(
+                name: "Actor_Movie",
+                newName: "Actors_Movies");
+
+            migrationBuilder.RenameTable(
+                name: "Actor",
+                newName: "Actors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Actor_Movie_MovieId",
+                table: "Actors_Movies",
+                newName: "IX_Actors_Movies_MovieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cinemas",
+                table: "Cinemas",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actors_Movies",
+                table: "Actors_Movies",
+                columns: new[] { "ActorId", "MovieId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actors",
+                table: "Actors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Actors_ActorId",
+                table: "Actors_Movies",
+                column: "ActorId",
+                principalTable: "Actors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Actors_Movies_Movie_MovieId",
+                table: "Actors_Movies",
+                column: "MovieId",
+                principalTable: "Movie",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Cinemas_CinemaId",
+                table: "Movie",
+                column: "CinemaId",
+                principalTable: "Cinemas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
